bd567c2b7ca662b60e39cc591233abce71c32648
AlxBlzrv <alxblzrv@gmail.com>
2024-03-24 13:26:22 +0200

    New repo Udemy projects

---
+++ Feature-Engineering&Linear-Regression/Dealing-with-Outliers.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Reading data
+df = pd.read_csv("D:/work/git/udemy/Feature-Engineering&Linear-Regression/Ames_Housing_Data.csv")
+
+# Heatmap correlation
+sns.heatmap(df.corr(numeric_only=True))
+plt.title('Heatmap of Correlation')
+plt.savefig("D:/work/git/udemy/Feature-Engineering&Linear-Regression/heatmap_corr.png")
+plt.close()
+
+# Output Sale Price correlation
+print(df.corr(numeric_only=True)['SalePrice'].sort_values())
+
+# Histogram with kernel density estimation
+sns.displot(df["SalePrice"], kde=True)
+plt.title('Distribution of SalePrice')
+plt.savefig("D:/work/git/udemy/Feature-Engineering&Linear-Regression/displot.png")
+plt.close()
+
+# Scatterplots with outliers
+sns.scatterplot(x='Overall Qual', y='SalePrice', data=df)
+plt.title('Scatterplot of Overall Qual vs SalePrice with outliers')
+plt.savefig("D:/work/git/udemy/Feature-Engineering&Linear-Regression/scatterplot1_with_outliers.png")
+plt.close()
+
+print(df[(df['Overall Qual']>8) & (df['SalePrice']<200000)])
+
+sns.scatterplot(x='Gr Liv Area', y='SalePrice', data=df)
+plt.title('Scatterplot of Gr Liv Area vs SalePrice with outliers')
+plt.savefig("D:/work/git/udemy/Feature-Engineering&Linear-Regression/scatterplot2_with_outliers.png")
+plt.close()
+
+print(df[(df['Gr Liv Area']>4000) & (df['SalePrice']<400000)])
+print(df[(df['Gr Liv Area']>4000) & (df['SalePrice']<400000)].index)
+
+# Removing outliers
+ind_drop = df[(df['Overall Qual'] > 8) & (df['SalePrice'] < 200000)].index
+df = df.drop(ind_drop, axis=0)
+
+ind_drop = df[(df['Gr Liv Area'] > 4000) & (df['SalePrice'] < 400000)].index
+df = df.drop(ind_drop, axis=0)
+
+# Scatterplots without outliers
+sns.scatterplot(x='Gr Liv Area', y='SalePrice', data=df)
+plt.title('Scatterplot of Gr Liv Area vs SalePrice without outliers')
+plt.savefig("D:/work/git/udemy/Feature-Engineering&Linear-Regression/scatterplot1_without_outliers.png")
+plt.close()
+
+sns.scatterplot(x='Overall Qual', y='SalePrice', data=df)
+plt.title('Scatterplot of Overall Qual vs SalePrice without outliers')
+plt.savefig("D:/work/git/udemy/Feature-Engineering&Linear-Regression/scatterplot2_without_outliers.png")
+plt.close()
+
+# save DataFrame
+df.to_csv("D:/work/git/udemy/Feature-Engineering&Linear-Regression/Ames_outliers_removed.csv", index=False)
